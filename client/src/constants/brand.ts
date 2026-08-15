0299988a38f52fe699735a24bbf44d3e2106a884
Landon-doit <1525898472@qq.com>
2026-08-15 16:58:47 +0800

    添加品牌名称和logo

--- client/src/constants/brand.ts
+++ client/src/constants/brand.ts
@@ -1,7 +1,7 @@
 export const BRAND = {
   name: '时澜集观',
   nameEn: 'Timelune',
-  logo: '/logo.svg', // public 路径
+  logo: '/logo.png', // public 路径
   primaryColor: '#FF6B35',
   accentColor: '#D4520A',
   highlightColor: '#FFD23F',
